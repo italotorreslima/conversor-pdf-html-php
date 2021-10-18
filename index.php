3bf7b09039d682b9fbabb467c964ebb7da1c9279
Italo <italotorreslima2002@gmail.com>
2021-10-18 18:57:26 -0300

    feat: index com loop gerador do html

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
-$command = escapeshellcmd('conversor.py');
-echo '<img style="width: 100%;" width="1080" src="0imagem.jpg">';
-echo '<br>';
-echo '<img style="width: 100%;" width="1080" src="1imagem.jpg">';
-echo '<br>';
-echo '<img style="width: 100%;" width="1080" src="2imagem.jpg">';
-echo '<br>';
+
+$output = shell_exec("python3 conversor.py");
+echo ($output);
+$imagick = new Imagick();
+$imagick->readImage('myfile.pdf');
+$pages = $imagick->getNumberImages();
+$myfile = fopen("index.html", "w");
+$txt = "<title>HTML PAGE</title>";
+fwrite($myfile, $txt);
+for ($i = 0; $i < $pages; $i++) {
+    $txt = "<img style=\"width: 100%;\"width=\"1080\" src=\"{$i}imagem.jpg\">";
+    fwrite($myfile, $txt);
+}
+fclose($myfile);
---
+++ readme.md
@@ -0,0 +1,1 @@
+### Biblioteca fitz do python nao esta funcionando no apache, mas quando roda através do CLI o comando "php index.php" funciona normalmente a conversão e a criação da pagina html
